fe23b1961565b70fd6f5a472dc2a23c6925e1765
Lavish Palia <lavishgarg1199@gmail.com>
2021-02-15 17:37:35 +0530

    static folders

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const connectDB = require('./db/db')
-const Meme = require('./models/meme')
+const connectDB = require('./db/db');
+const Meme = require('./models/meme');
 const app = express();
 
 connectDB();
@@ -11,55 +12,59 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
+app.get('/memes', async (req, res) => {
+	const memes = await Meme.find();
 
+	if (!memes) {
+		return res.send(404).json({ msg: 'No memes available' });
+	}
+	res.status(200).json(memes);
+});
 
-app.get('/memes', async(req, res) => {
-    const memes = await Meme.find();
+app.get('/memes/:mid', async (req, res) => {
+	const memeId = req.params.mid;
 
-    if(!memes) {
-        return res.send(404).json(
-            {msg: 'No memes available'}
-        )
-    }
-    res.status(200).json(memes)
-})
+	const meme = await Meme.findById({ _id: memeId });
 
-app.get('/memes/:mid', async(req, res) => {
-    const memeId = req.params.mid;
+	if (!meme) {
+		return res.status(404).json({ msg: 'No meme found for the provided id.' });
+	}
+	res.status(200).json({ meme: meme });
+});
 
-    const meme = await Meme.findById({_id: memeId});
+app.post('/memes', async (req, res) => {
+	const { name, caption, url } = req.body;
+	console.log(name, caption, url);
 
-    if(!meme) {
-       return res.status(404).json({ msg: 'No meme found for the provided id.'})
-    }
-    res.status(200).json({meme: meme})
-})
+	const isName = await Meme.findOne({ name });
+	const isCaption = await Meme.findOne({ caption });
+	const isUrl = await Meme.findOne({ url });
 
-app.post('/memes', async(req, res) => {
-    const { name, caption, url } = req.body;
-    console.log(name, caption, url);
+	if (isName && isCaption && isUrl) {
+		res.status(409).json({ msg: 'Meme exists already.' });
+	}
 
-    const isName = await Meme.findOne({name})
-    const isCaption = await Meme.findOne({caption})
-    const isUrl = await Meme.findOne({url})
+	try {
+		const meme = await new Meme({
+			name,
+			caption,
+			url,
+		});
 
-    if(isName && isCaption && isUrl) {
-        res.status(409).json({ msg: 'Meme exists already.'})
-    }
+		await meme.save();
+		res.status(201).json({ meme: meme });
+	} catch (error) {
+		res.status(500).json({ msg: 'Server error' });
+		console.error(error);
+	}
+});
 
-    try {
-        const meme = await new Meme({
-            name,
-            caption,
-            url
-        })
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 
-        await meme.save();
-        res.status(201).json({ meme: meme})
-    } catch (error) {
-        res.status(500).json({ msg: 'Server error'});
-        console.error(error)
-    }
-})
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
-app.listen(PORT)
+app.listen(PORT);
